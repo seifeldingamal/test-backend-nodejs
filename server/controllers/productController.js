573dffc9a3ea0c0f48fd0fd4c5105fb53b07099d
Seifeldin Abouelella <seifaldin.aboelella@gmail.com>
2022-01-03 21:49:24 +0200

    Created the rest of the required Routes

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -5,6 +5,15 @@ module.exports.productsGet = async (req, res) => {
     res.send(products);
 }
 
+module.exports.productsFilter = async (req, res) => {
+
+    const products = await Product.find({
+        $or: [{title: req.body.title}, {category: req.body.category}]
+    });
+    
+    res.send(products);
+}
+
 module.exports.productPost = async (req, res) => {
 	const product = new Product({
         title: req.body.title,
@@ -14,4 +23,38 @@ module.exports.productPost = async (req, res) => {
     });
 	await product.save();
 	res.send(product);
+}
+
+module.exports.productPut = (req, res) => {
+    let updates = req.body 
+
+    Product.findOneAndUpdate
+        ({ _id: req.params.id }, updates)
+      .then
+        (updatedProduct => res.json(updatedProduct))
+      .catch
+        (err => res.status(400).json("Error: " + err))
+}
+
+module.exports.categoryUpdate = (req, res) => {
+    let newCat = req.body.category 
+
+    Product.findOneAndUpdate
+        ({ _id: req.params.id }, {
+            category: newCat
+        })
+      .then
+        (updatedProduct => res.json(updatedProduct))
+      .catch
+        (err => res.status(400).json("Error: " + err))
+}
+
+
+module.exports.productDelete = (req, res) => {
+    Product.findByIdAndDelete
+        (req.params.id)
+    .then
+        (() => res.json("Product deleted!"))
+    .catch
+        (err => res.status(400).json("Error: " + err))
 }
--- server/routes/products.js
+++ server/routes/products.js
@@ -6,6 +6,19 @@ const router = express.Router();
 // Get Products
 router.get('/', productsController.productsGet);
 
+// Get Filtered Products
+router.get('/filter', productsController.productsFilter);
+
+// Add Product
 router.post('/', productsController.productPost);
+
+// Update Product
+router.put('/:id/update', productsController.productPut);
+
+// Update Product Category
+router.put('/:id/category', productsController.categoryUpdate);
+
+// Delete
+router.delete('/:id/delete', productsController.productDelete);
 
 module.exports = router;
