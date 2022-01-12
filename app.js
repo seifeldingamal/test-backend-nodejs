05fd9b17d3d9a08217ccc2119bc7d52ab588eee5
Seifeldin Abouelella <seifaldin.aboelella@gmail.com>
2022-01-12 19:05:56 +0200

    API Documentation using Swagger Implemented

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { connect } = require("./db")
+const { connect } = require("./db");
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./docs/products.yaml');
 
 const app = express();
 
@@ -11,6 +14,8 @@ connect()
     // Middleware
     app.use(bodyParser.json());
     app.use(cors());
+
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
     const noAuthRoutes = require("./routes/noauth/routes")
     app.use('/noauth', noAuthRoutes);
